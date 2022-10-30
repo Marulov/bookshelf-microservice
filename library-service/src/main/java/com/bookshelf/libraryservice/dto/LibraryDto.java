cd59b45af1df012c8efcda8386ee2cc9a34b527a
Marulov <nazmmarul@hotmail.com>
2022-10-30 16:07:15 +0300

    FeignClient add to LibraryService.

--- library-service/src/main/java/com/bookshelf/libraryservice/dto/LibraryDto.java
+++ library-service/src/main/java/com/bookshelf/libraryservice/dto/LibraryDto.java
@@ -1,12 +1,16 @@
 package com.bookshelf.libraryservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LibraryDto {
     private Long id;
     private List<BookDto> userBooks;
--- library-service/src/main/java/com/bookshelf/libraryservice/service/LibraryService.java
+++ library-service/src/main/java/com/bookshelf/libraryservice/service/LibraryService.java
@@ -1,12 +1,17 @@
 package com.bookshelf.libraryservice.service;
 
+import com.bookshelf.libraryservice.client.BookServiceClient;
 import com.bookshelf.libraryservice.converter.LibraryDtoConverter;
+import com.bookshelf.libraryservice.dto.AddBookRequest;
 import com.bookshelf.libraryservice.dto.LibraryDto;
 import com.bookshelf.libraryservice.exception.LibraryNotFoundException;
 import com.bookshelf.libraryservice.model.Library;
 import com.bookshelf.libraryservice.repository.LibraryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,15 +20,32 @@ public class LibraryService {
     private final LibraryRepository libraryRepository;
     private final LibraryDtoConverter libraryDtoConverter;
 
+    private final BookServiceClient bookServiceClient;
+
     public LibraryDto getAllBooksInLibraryById(Long id) {
         Library library = libraryRepository.findById(id)
                 .orElseThrow(() -> new LibraryNotFoundException("Library not found by id: " + id));
 
-        return libraryDtoConverter.convert(library);
+        return new LibraryDto(library.getId(),
+                library.getUserBooks()
+                        .stream()
+                        .map(x -> bookServiceClient.getBookDetailsById(Long.valueOf(x)))
+                        .map(ResponseEntity::getBody)
+                        .collect(Collectors.toList()));
     }
 
     public LibraryDto createLibrary() {
         Library newLibrary = libraryRepository.save(new Library());
         return libraryDtoConverter.convert(newLibrary);
+    }
+
+    public void addBookToLibrary(AddBookRequest request) {
+        Long bookId = bookServiceClient.getBookByIsbn(request.getIsbn()).getBody().getId();
+
+        Library library = libraryRepository.findById(request.getId())
+                .orElseThrow(() -> new LibraryNotFoundException("Library not found by id: " + request.getId()));
+
+        library.getUserBooks().add(String.valueOf(bookId));
+        libraryRepository.save(library);
     }
 }
